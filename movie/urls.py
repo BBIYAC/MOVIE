900a9a91e92fd2bdc8665544a5aa1d97b7564ddd
inyeong <kiy7833@gmail.com>
2021-08-13 13:33:21 +0900

    Add loading

--- movie/urls.py
+++ movie/urls.py
@@ -5,9 +5,9 @@ import movie.views as view
 
 urlpatterns = [
     path('index/', view.index, name='index'),
-    path('', view.rank, name='rank'),
+    path('rank/', view.rank, name='rank'),
     path('near-theater/', view.nearTheater, name='nearTheater'),
     path('timetable/', view.timetable, name='timetable'),
-    path('location/', view.location, name='location'),
+    path('', view.location, name='location'),
     path('selectseat/', view.selectseat, name='selectseat'),
 ]
